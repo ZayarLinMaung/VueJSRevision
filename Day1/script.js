b2c4cd44641e94f2ee6f20956f0a5d1970e96862
Kira-god <kirayuzo189@gmail.com>
2024-04-14 23:27:59 +0630

    first commit

---
+++ Day1/script.js
@@ -0,0 +1,26 @@
+alert("Hello World");
+console.log("Hello World");
+let usrName = "ZLM";
+console.log(usrName);
+usrName = "Zayar Lin Maung";
+console.log(usrName);
+const pi = 3.14;
+console.log(pi);
+// pi = 3.15;
+// console.log(object);
+//DataTypes
+//String
+let str = "ZLM";
+//number
+let num = 1;
+//boolean
+let isTrue = true;
+//null => nothing
+let emptyContainer = null;
+//undefined
+let unknownVar;
+console.log(unknownVar);
+//operator
+//+, -, *, /, %
+// >, <, >=, <=, ==, ===, !==
+// =, +=, -=, *=, /=, %=
